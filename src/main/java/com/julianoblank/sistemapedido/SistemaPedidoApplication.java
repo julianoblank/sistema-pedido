d5170993d7389caeef58409cb8356dfc1a37aebb
Juliano Blank <julianoellwanger@gmail.com>
2023-03-08 09:59:03 -0300

    Criando operacao de instanciacao

--- src/main/java/com/julianoblank/sistemapedido/SistemaPedidoApplication.java
+++ src/main/java/com/julianoblank/sistemapedido/SistemaPedidoApplication.java
@@ -1,13 +1,33 @@
 package com.julianoblank.sistemapedido;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.julianoblank.sistemapedido.domain.Categoria;
+import com.julianoblank.sistemapedido.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaPedidoApplication {
+public class SistemaPedidoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
