3fe1cf348c636003c40cc852864cf936f645ce37
Juliano Blank <julianoellwanger@gmail.com>
2023-03-08 10:29:07 -0300

    Produto e associacao muito para muitos

--- src/main/java/com/julianoblank/sistemapedido/SistemaPedidoApplication.java
+++ src/main/java/com/julianoblank/sistemapedido/SistemaPedidoApplication.java
@@ -9,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.julianoblank.sistemapedido.domain.Categoria;
+import com.julianoblank.sistemapedido.domain.Produto;
 import com.julianoblank.sistemapedido.repositories.CategoriaRepository;
+import com.julianoblank.sistemapedido.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SistemaPedidoApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidoApplication.class, args);
@@ -23,10 +28,25 @@ public class SistemaPedidoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
