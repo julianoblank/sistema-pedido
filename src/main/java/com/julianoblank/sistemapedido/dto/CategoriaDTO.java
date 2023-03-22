63ed16cb58ddd1e553506a61f023716fe74ad464
Juliano Blank <julianoellwanger@gmail.com>
2023-03-22 10:25:52 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/julianoblank/sistemapedido/dto/CategoriaDTO.java
+++ src/main/java/com/julianoblank/sistemapedido/dto/CategoriaDTO.java
@@ -2,7 +2,14 @@ package com.julianoblank.sistemapedido.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.julianoblank.sistemapedido.domain.Categoria;
+
+
+
 
 public class CategoriaDTO implements Serializable{
 	
@@ -10,6 +17,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
+++ src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julianoblank.sistemapedido.domain.Categoria;
+import com.julianoblank.sistemapedido.dto.CategoriaDTO;
 import com.julianoblank.sistemapedido.repositories.CategoriaRepository;
 import com.julianoblank.sistemapedido.services.exceptions.DataIntegrityException;
 import com.julianoblank.sistemapedido.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
