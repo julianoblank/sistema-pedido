9993cd07c9c9f976b3c31805c930cabf44b6acee
Juliano Blank <julianoblank@unisc.br>
2023-03-24 11:06:26 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
+++ src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julianoblank.sistemapedido.domain.Categoria;
+import com.julianoblank.sistemapedido.domain.Cliente;
 import com.julianoblank.sistemapedido.dto.CategoriaDTO;
 import com.julianoblank.sistemapedido.repositories.CategoriaRepository;
 import com.julianoblank.sistemapedido.services.exceptions.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -61,6 +63,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
