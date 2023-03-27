c1c16fadd9614e332fdcf1ea160791f7293a8c25
Juliano Blank <julianoblank@unisc.br>
2023-03-27 11:01:36 -0300

    Validacao customizada de CPF ou CNPJ na inserção de cliente

--- src/main/java/com/julianoblank/sistemapedido/resources/exception/ValidationError.java
+++ src/main/java/com/julianoblank/sistemapedido/resources/exception/ValidationError.java
@@ -7,13 +7,13 @@ public class ValidationError extends StandardError{
 
 	private static final long serialVersionUID = 1L;
 	
-	private List<FieldMessage> errors = new ArrayList<>();
+	private List<FieldMessage> errors = new ArrayList<>(); // tem essa nome, porque o springboot utiliza ela para mostrar quando tem erro.
 	
 	public ValidationError(Integer status, String msg, Long timeStamp) {
 		super(status, msg, timeStamp);
 	}
 
-	public List<FieldMessage> getErrors() {
+	public List<FieldMessage> getErrors() { // tem essa assinatura, porque o springboot utiliza ela para mostrar quando tem erro.
 		return errors;
 	}
 
